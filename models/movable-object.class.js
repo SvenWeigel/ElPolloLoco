2f827d2de4d2cb4cd04e0d939f8b908d617b1b87
Sven <sven.wgl@gmail.com>
2026-02-12 20:56:42 +0100

    if engery hit 0 fall animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -16,7 +16,7 @@ class MovableObject extends DrawableObject {
   }
 
   isAboveGround() {
-    if (this instanceof ThrowableObject) {
+    if (this instanceof ThrowableObject || this.energy == 0) {
       return true;
     } else {
       return this.y < 150;
