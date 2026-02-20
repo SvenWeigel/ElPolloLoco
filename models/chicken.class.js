55388a8356628d6740dfa6ddbe3cec5a4e81fc8f
Sven <sven.wgl@gmail.com>
2026-02-20 11:24:36 +0100

    fix: chicken health

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,7 +2,7 @@ class Chicken extends MovableObject {
   y = 360;
   height = 65;
   width = 80;
-  energy = 40;
+  energy = 20;
 
   IMAGES_WALKING = [
     "assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
