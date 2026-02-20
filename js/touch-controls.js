647f54717c8b552bdf349177cbfdacec0eb2e32d
Sven <sven.wgl@gmail.com>
2026-02-20 16:51:33 +0100

    change: touch controll function

--- js/touch-controls.js
+++ js/touch-controls.js
@@ -1,49 +1,24 @@
+function addTouchHandlers(btn, key) {
+  btn.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard[key] = true;
+  });
+  btn.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard[key] = false;
+  });
+}
+
 function initTouchControls() {
   const leftBtn = document.getElementById("left-btn");
-  if (leftBtn) {
-    leftBtn.addEventListener("touchstart", (e) => {
-      e.preventDefault();
-      keyboard.LEFT = true;
-    });
-    leftBtn.addEventListener("touchend", (e) => {
-      e.preventDefault();
-      keyboard.LEFT = false;
-    });
-  }
+  if (leftBtn) addTouchHandlers(leftBtn, "LEFT");
 
   const rightBtn = document.getElementById("right-btn");
-  if (rightBtn) {
-    rightBtn.addEventListener("touchstart", (e) => {
-      e.preventDefault();
-      keyboard.RIGHT = true;
-    });
-    rightBtn.addEventListener("touchend", (e) => {
-      e.preventDefault();
-      keyboard.RIGHT = false;
-    });
-  }
+  if (rightBtn) addTouchHandlers(rightBtn, "RIGHT");
 
   const jumpBtn = document.getElementById("jump-btn");
-  if (jumpBtn) {
-    jumpBtn.addEventListener("touchstart", (e) => {
-      e.preventDefault();
-      keyboard.SPACE = true;
-    });
-    jumpBtn.addEventListener("touchend", (e) => {
-      e.preventDefault();
-      keyboard.SPACE = false;
-    });
-  }
+  if (jumpBtn) addTouchHandlers(jumpBtn, "SPACE");
 
   const throwBtn = document.getElementById("throw-btn");
-  if (throwBtn) {
-    throwBtn.addEventListener("touchstart", (e) => {
-      e.preventDefault();
-      keyboard.D = true;
-    });
-    throwBtn.addEventListener("touchend", (e) => {
-      e.preventDefault();
-      keyboard.D = false;
-    });
-  }
+  if (throwBtn) addTouchHandlers(throwBtn, "D");
 }
